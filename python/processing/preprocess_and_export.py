48a9611cbe1d861cd6c2d247eac9c13a7e51bf6a
Thomas Johansen <thomasjo@gmail.com>
2019-03-24 13:32:34 +0100

    Improve the preprocess and export script

--- python/processing/preprocess_and_export.py
+++ python/processing/preprocess_and_export.py
@@ -1,3 +1,22 @@
+"""
+preprocess_and_export.py
+
+Usage:
+  preprocess_and_export.py [--data-dir=<path>] [--output-dir=<path>]
+                           [--skip-existing=<bool>]
+
+Options:
+  -h --help               Show this screen.
+  --data-dir=<path>       The source directory to recursively glob for raw
+                          hyperspectral cubes to process.
+                          [default: /root/data/HSPC1]
+  --output-dir=<path>     The output directory for all processed cubes.
+                          [default: /root/output/processed]
+  --skip-existing=<bool>  Skip files that have already been processed.
+                          [default: true]
+"""
+
+import sys
 import warnings
 
 import fpipy as fpi
@@ -7,94 +26,81 @@ import matplotlib.collections as collections
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 
+from distutils.util import strtobool
+from docopt import docopt
 from pathlib import Path
 
 
-data_dir = Path("/root/data/HSPC1/MM")
-output_dir = Path("/root/output")
+row_crop = slice(160, 1160)
+col_crop = slice(500, 1500)
 
-# Select a hyperspectral skin lesion image.
-cube_dir = data_dir / "a965ccdcc83d466386649b1a21a927b1078a71bb"
-# cube_dir = data_dir / "775d2a3131b92553009b2165e2cc4ee323b84c71"
-# cube_dir = data_dir / "7bf233f5848c413fca645cc38b99283ab6d88641"
 
-# Load the raw measurements of the lesion image, and convert to radiance.
-raw_file = cube_dir / "RawMeasurementCube.hdr"
+def _radiance_cube(hdr_file):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        raw_dataset = fpi.read_ENVI_cfa(str(hdr_file))
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    raw_dataset = fpi.read_ENVI_cfa(str(raw_file))
+    rad_dataset = fpi.raw_to_radiance(raw_dataset)
+    return rad_dataset.radiance.values[row_crop, col_crop].copy()
 
-rad_dataset = fpi.raw_to_radiance(raw_dataset)
-rad_cube = rad_dataset.radiance.values
 
-print('rad_cube:')
-print(rad_cube.min(), rad_cube.max())
-print(rad_cube[200:1000, 500:1500].min(), rad_cube[200:1000, 500:1500].max())
-print()
+def process_directory(data_dir, output_dir, skip_existing=True):
+    for raw_file in data_dir.rglob("RawMeasurementCube.hdr"):
+        cube_dir = raw_file.parents[0]
+        category = raw_file.parents[1].name
 
-# Load the white reference image captured prior to capturing the lesion image,
-# and convert to radiance.
-raw_file = cube_dir / "WhiteReference.hdr"
+        output_file = output_dir / category / "{}.npy".format(cube_dir.name)
+        output_file.parent.mkdir(parents=True, exist_ok=True)
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    raw_dataset = fpi.read_ENVI_cfa(str(raw_file))
+        if skip_existing and output_file.exists():
+            print("  skipping: {}".format(raw_file))
+            continue
 
-ref_dataset = fpi.raw_to_radiance(raw_dataset)
-ref_cube = ref_dataset.radiance.values
+        print("processing: {}".format(raw_file))
 
-print('ref_cube:')
-print(ref_cube.min(), ref_cube.max())
-print(ref_cube[160:1160, 500:1500].min(), ref_cube[160:1160, 500:1500].max())
-print()
+        # Load the raw measurements of the lesion image, and convert to
+        # radiance.
+        rad_cube = _radiance_cube(raw_file)
 
-# Crop the lesion image to remove lens cover, etc.
-cropped = rad_cube[160:1160, 500:1500]
+        # Load the white reference image captured prior to capturing the lesion
+        # image, and convert to radiance.
+        ref_file = cube_dir / "WhiteReference.hdr"
+        ref_cube = _radiance_cube(ref_file)
 
-print('cropped:')
-print(cropped.min(), cropped.max())
-print(np.unravel_index(cropped.argmin(), cropped.shape))
-print(np.unravel_index(cropped.argmax(), cropped.shape))
-print()
+        # Calculate the normalized cube using the white reference cube.
+        normalized = (rad_cube / ref_cube).copy()
+        del rad_cube
+        del ref_cube
 
-# Calculate the normalized cube using the white reference cube.
-normalized = cropped / ref_cube[160:1160, 500:1500]
+        # Remove noisy wavelengths in the NIR range.
+        # normalized = normalized[..., 0:115]
 
-print('normalized:')
-print(normalized.min(), normalized.max())
-print(np.unravel_index(normalized.argmin(), normalized.shape))
-print(np.unravel_index(normalized.argmax(), normalized.shape))
-print()
+        # Threshold normalized values to [0, 1].
+        # normalized[normalized < 0] = 0
+        # normalized[normalized > 1] = 1
 
-# Remove noisy wavelengths in the NIR range.
-normalized = normalized[..., 0:115]
+        # Save processed cube as NumPy array.
+        np.save(str(output_file), normalized)
 
-print('normalized (subset):')
-print(normalized.min(), normalized.max())
-print(np.unravel_index(normalized.argmin(), normalized.shape))
-print(np.unravel_index(normalized.argmax(), normalized.shape))
-print()
+        # Save RGB image of skin lesion.
+        rgb_file = cube_dir / "RGB_Image.png"
+        if rgb_file.exists():
+            rgb_image = plt.imread(str(rgb_file))
+            rgb_cropped = rgb_image[row_crop, col_crop]
+            rgb_output_file = output_file.with_suffix(".png")
+            plt.imsave(str(rgb_output_file), rgb_cropped)
 
-# Threshold normalized values to [0, 1].
-normalized[normalized < 0] = 0
-normalized[normalized > 1] = 1
 
-print('normalized (clamped):')
-print(normalized.min(), normalized.max())
-print(np.unravel_index(normalized.argmin(), normalized.shape))
-print(np.unravel_index(normalized.argmax(), normalized.shape))
-print()
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    data_dir = Path(args["--data-dir"])
+    output_dir = Path(args["--output-dir"])
+    skip_existing = strtobool(args["--skip-existing"])
 
-# Plot RGB image of skin lesion.
-file_path = cube_dir / 'RGB_Image.png'
-rgb_image = plt.imread(str(file_path))
-rgb_cropped = rgb_image[160:1160, 500:1500]
+    print()
+    print("Source directory: {}".format(data_dir))
+    print("Output directory: {}".format(output_dir))
 
-# plt.figure()
-# plt.imshow(rgb_cropped)
-# plt.savefig(str(output_dir / 'rgb.png'))
-plt.imsave(str(output_dir / 'rgb.png'), rgb_cropped)
-
-npy_file = output_dir / 'processed.npy'
-np.save(str(npy_file), normalized)
+    print("-" * 72)
+    process_directory(data_dir, output_dir, skip_existing)
+    print("-" * 72)
