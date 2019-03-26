7d354973788db7390f232e163a095e926ece9705
Thomas Johansen <thomasjo@gmail.com>
2019-03-26 12:34:28 +0100

    Move execution logic into __main__ block

--- python/experiments/isicw2019/cluster_patches.py
+++ python/experiments/isicw2019/cluster_patches.py
@@ -88,59 +88,59 @@ def _cache_id(*args):
     return shasum[:7]
 
 
-shasum_crop = {
-    "775d2a3": (340, 360),
-    "1425595": (364, 316),
-    "f19607d": (260, 318),
-    "766c5be": (169, 428),
-}
+if __name__ == "__main__":
+    shasum_crop = {
+        "775d2a3": (340, 360),
+        "1425595": (364, 316),
+        "f19607d": (260, 318),
+        "766c5be": (169, 428),
+    }
 
-distances = {"euclidean": _euclidean, "ecs": _ecs}
+    distances = {"euclidean": _euclidean, "ecs": _ecs}
+    dist_colors = {"euclidean": (1, 0, 0), "ecs": (0, 1, 1)}
 
-dist_colors = {"euclidean": (1, 0, 0), "ecs": (0, 1, 1)}
+    # shasum, xy = next(iter(shasum_crop.items()))
+    for shasum, crop_xy in shasum_crop.items():
+        image_file = find_image_file(shasum)
+        image_data, rgb_image = load_image(image_file, crop_xy)
 
-# shasum, xy = next(iter(shasum_crop.items()))
-for shasum, crop_xy in shasum_crop.items():
-    image_file = find_image_file(shasum)
-    image_data, rgb_image = load_image(image_file, crop_xy)
+        print("Processing: ", image_file)
 
-    print("Processing: ", image_file)
+        # NOTE: Check that we've cropped the correct region. ------------------
+        debug_file = output_dir / "{}-debug.png".format(image_file.stem)
+        plt.imsave(str(debug_file), image_data[..., -1])
+        # ---------------------------------------------------------------------
 
-    # NOTE: Check that we've cropped the correct region. ----------------------
-    debug_file = output_dir / "{}-debug.png".format(image_file.stem)
-    plt.imsave(str(debug_file), image_data[..., -1])
-    # -------------------------------------------------------------------------
+        m, n, k = image_data.shape
+        pixels = image_data.reshape((-1, k))
 
-    m, n, k = image_data.shape
-    pixels = image_data.reshape((-1, k))
+        comparison_image = rgb_image.copy()
+        print(comparison_image.shape)
 
-    comparison_image = rgb_image.copy()
-    print(comparison_image.shape)
+        for name, dist_fn in distances.items():
+            print("  Segmenting with {} metric...".format(name))
 
-    for name, dist_fn in distances.items():
-        print("  Segmenting with {} metric...".format(name))
+            cache_id = _cache_id(shasum, crop_xy, name)
+            cache_file = cache_dir / "{}.npy".format(cache_id)
+            if cache_file.exists():
+                print("    Using cached cluster labels")
+                labels = np.load(cache_file)
+            else:
+                print("    Computing cluster labels...")
+                labels = segment_pixels(pixels, dist_fn)
+                np.save(cache_file, labels)
 
-        cache_id = _cache_id(shasum, crop_xy, name)
-        cache_file = cache_dir / "{}.npy".format(cache_id)
-        if cache_file.exists():
-            print("    Using cached cluster labels")
-            labels = np.load(cache_file)
-        else:
-            print("    Computing cluster labels...")
-            labels = segment_pixels(pixels, dist_fn)
-            np.save(cache_file, labels)
+            label_image = labels.reshape((m, n))
 
-        label_image = labels.reshape((m, n))
+            color = dist_colors.get(name, (1, 1, 0))
+            comparison_image = mark_boundaries(comparison_image, label_image, color)
 
-        color = dist_colors.get(name, (1, 1, 0))
-        comparison_image = mark_boundaries(comparison_image, label_image, color)
+            # TODO: Extract as function.
+            fig = plt.figure()
+            ax1, ax2 = fig.subplots(1, 2)
+            ax1.imshow(rgb_image)
+            ax2.imshow(label_image)
+            plt.savefig(str(output_dir / "{}-{}.png".format(image_file.stem, name)))
 
-        # TODO: Extract as function.
-        fig = plt.figure()
-        ax1, ax2 = fig.subplots(1, 2)
-        ax1.imshow(rgb_image)
-        ax2.imshow(label_image)
-        plt.savefig(str(output_dir / "{}-{}.png".format(image_file.stem, name)))
-
-    comparison_file = output_dir / "{}-cmp.png".format(image_file.stem)
-    plt.imsave(str(comparison_file), comparison_image)
+        comparison_file = output_dir / "{}-cmp.png".format(image_file.stem)
+        plt.imsave(str(comparison_file), comparison_image)
